52d315d10ecadfc212e21535b8875e22f8a07fb2
Sandeep Samant <sandeepsamant1702@gmail.com>
2017-10-11 00:58:54 +0530

    Random Colors Added

--- col.js
+++ col.js
@@ -1,15 +1,10 @@
-var colors = [
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedcolor = colors[3];
+var pickedcolor = pickColor();
 var colorDisplay= document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1=document.querySelector("h1");
 
 colorDisplay.textContent = pickedcolor;
 
@@ -26,14 +21,67 @@ for(var i =0; i<squares.length; i++)
 	console.log(clickedColor + pickedcolor);
 	//compare color to pickedcolor
 	if(clickedColor === pickedcolor){
-		alert("Correct");
+		messageDisplay.textContent="Correct";
+		changeColors(pickedcolor);
+		h1.style.background = clickedColor;
 	}
 	else
 	{
 		alert("WRONG!!");
+		this.style.background= "#232323";
+		messageDisplay.textContent="Try Again";
 	}
 
 	
 	
 });
 }
+
+function changeColors(color){
+	//loop through all squares
+	for(var i =0; i<squares.length; i++)
+	{
+	//change each color to match given color
+	squares[i].style.background = color;
+	
+
+	}
+}
+
+function pickColor()
+{
+  var random = Math.floor(Math.random()*colors.length);
+  return colors[random];	
+}
+
+function generateRandomColors(num)
+{
+  //make an array
+  var arr = []
+  //repeat num times
+  for(var i=0; i <num; i++){
+  	//get random color and push into arr
+  	arr.push(randomColor());
+  }
+  //return that array
+  return arr;
+
+}
+
+function randomColor(){
+	//pick a red from 0-255
+	var r = Math.floor(Math.random()*256);
+	//pick a green from 0-255
+	var g = Math.floor(Math.random()*256);
+	//pick a blue from 0-255
+	var b = Math.floor(Math.random()*256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+
+
+	
+
+
+}
+
+
